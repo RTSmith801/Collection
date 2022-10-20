c45952e224b3819e0449982ee8ea59cd2d30ba7d
Rick <RTSmith801@gmail.com>
2022-10-19 20:17:33 -0600

    Reformatting .h & .cpp files

--- Collection/Collection/Collection.cpp
+++ Collection/Collection/Collection.cpp
@@ -1,5 +1,66 @@
+/**
+ * @file: Collection.cpp
+ * @assignment_name:  01: Try it out Activity: Collection
+ * @author: Rick Smith (rickysmith@mail.weber.edu)
+ * @date: 10/19/2022
+ * @brief: program to display use of objects and simple arrays
+ *
+ */
+
 #include "Collection.h"
 
-Collection::Collection()
-{
+//constructor
+//argument constructor that takes an integer parameter and uses it to set the initial capacity of the array
+Collection::Collection(int size) {
+	if (size < 1) {
+		//check for valid array		
+		size = 1;
+	}
+	
+	double myArray[size];
+	
 }
+
+// getters
+//returns the number of elements in the array.Unlike c - strings where we had a null - terminator to mark the end of the array, 
+//in this case you will need a variable to keep track of the number of elements currently in the array.
+int Collection::getSize() { 
+	return sizeof(myArray);
+}
+
+//returns the maximum number of elements allowed in the current array.
+int Collection::getCapacity(){
+	int capacity = 0;
+	return capacity;	
+}
+
+//Gets the value stored at the specified position. Throws and out_of_range exception if the index is outside the bounds of the array.
+double Collection::get(int ndx) {
+	
+}
+//Returns the first value in the array. Throws an out_of_range exception if the array is empty.
+double Collection::getFront() {}
+
+//Returns the last value in the array.Throws and out_of_range exception if the array is empty.
+double Collection::getEnd() {}
+
+// setters
+
+// other methods
+//As you add the value to the back of the array, you should also check update the size(i.e.the number of elements in the array).
+//If the new item exceeds the max size of the list, throw a runtime error...
+void Collection::add(double value) {}
+
+//throw runtime_error("List Full");
+//throw Collection::runtime_error("List Full") {}
+
+//This will add an item to the front of the list.If the new item exceeds the max size, throw a runtime_exception.
+void Collection::addFront(double value) {}
+
+//returns the position of needle in the list, -1 if not found
+int Collection::find(double needle) {}
+
+// friend methods	
+//overload the extraction operator to display the list.
+//friend std::ostream& operator<<(std::ostream& out, const Collection & c) {}
+
--- Collection/Collection/Collection.h
+++ Collection/Collection/Collection.h
@@ -1,33 +1,18 @@
 #ifndef COLLECTION_H
 #define COLLECTION_H
 
-/*
-
-	Collection(): Default constructor for the collection.  Initializes the array to a fixed size of your choice
-	Collection(int size): argument constructor that takes an integer parameter and uses it to set the initial capacity of the array
-	int getSize(): returns the number of elements in the array. Unlike c-strings where we had a null-terminator to mark the end of the array, in this case you will need a variable to keep track of the number of elements currently in the array.
-	int getCapacity(): returns the maximum number of elements allowed in the current array.
-	void add(double value)  As you add the value to the back of the array, you should also check update the size (i.e. the number of elements in the array).  If the new item exceeds the max size of the list, throw a runtime error...
-
-	throw runtime_error("List Full");
-
-	void addFront(double value): This will add an item to the front of the list.  If the new item exceeds the max size,  throw a runtime_exception.
-	double get(int ndx): Gets the value stored at the specified position. Throws and out_of_range exception if the index is outside the bounds of the array.
-	double getFront(): Returns the first value in the array. Throws an out_of_range exception if the array is empty.
-	double getEnd(): Returns the last value in the array. Throws and out_of_range exception if the array is empty.
-	int find(double needle): returns the position of needle in the list, -1 if not found
-	friend std::ostream& operator<<(std::ostream& out, const Collection & c): overload the extraction operator to display the list.
-*/
-
 class Collection{
 
-protected:
+private:	
+	int defaultSize_ = 5;
+	int arraySize_ = 0; 
 	
 
 public:
 	//constructors
-	Collection();
-	Collection(int size) {}
+	//Default constructor for the collection.Initializes the array to a fixed size of your choice
+	Collection() {double myArray[defaultSize_]};
+	Collection(int size);
 
 	// destructor
 	~Collection() {}
@@ -35,23 +20,23 @@ public:
 	// static members
 	
 	// getters
-	int getSize() {}
-	int getCapacity {}
-	double get(int ndx) {}
-	double getFront() {}
-	double getEnd() {}
+	int getSize();
+	int getCapacity();
+	double get(int ndx);
+	double getFront();
+	double getEnd();
 
 	// setters
 	
 	// other methods
-	void add(double value) {}
-	throw runtime_error("List Full") {}
-	void addFront(double value)	
+	void add(double value);
+	//throw runtime_error("List Full");
+	void addFront(double value);
 	
-	int find(double needle)
+	int find(double needle);
 
 	// friend methods	
-	friend std::ostream& operator<<(std::ostream& out, const Collection & c) {}
+	//friend std::ostream& operator<<(std::ostream& out, const Collection & c);
 
 }
 
